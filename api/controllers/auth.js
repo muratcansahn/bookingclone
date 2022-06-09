01cb74ebf12439f2797c70e2627ec112d6bb3111
muratcansahn <muratcansahin10@gmail.com>
2022-06-10 01:06:28 +0300

    add jwt

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,13 +1,46 @@
 import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 export const register = async (req, res, next) => {
   try {
+    var salt = bcrypt.genSaltSync(10);
+    var hash = bcrypt.hashSync(req.body.password, salt);
     const newUser = new User({
       username: req.body.username,
       email: req.body.email,
-      password: req.body.password,
+      password: hash,
     });
     await newUser.save();
     res.status(201).send("User has been created");
+  } catch (err) {
+    next(err);
+  }
+};
+export const login = async (req, res, next) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    console.log(user);
+    if (!user) {
+      res.status(401).send("User not found");
+    }
+    const isPasswordValid = bcrypt.compareSync(
+      req.body.password,
+      user.password
+    );
+    if (!isPasswordValid) {
+      res.status(401).send("Password is not valid");
+    }
+    const token = jwt.sign(
+      { id: user._id, isAdmin: user.isAdmin },
+      process.env.JWT
+    );
+
+    const { password, isAdmin, ...otherDetails } = user._doc;
+
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(201)
+      .json({ ...otherDetails });
   } catch (err) {
     next(err);
   }
--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import authRoute from "./routes/auth.js";
 import usersRoute from "./routes/users.js";
 import hotelsRoute from "./routes/hotels.js";
 import roomsRoute from "./routes/rooms.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 dotenv.config();
@@ -29,6 +30,7 @@ mongoose.connection.on("connected", () => {
 ///normally we cant send json object to express server prevent this we use this
 app.use(express.json());
 
+app.use(cookieParser());
 app.use("/api/auth", authRoute);
 app.use("/api/users", usersRoute);
 app.use("/api/hotels", hotelsRoute);
